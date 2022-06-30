0d25b123e234857e312eceb4311644696359e10a
FrostIce482 <frostice48260@gmail.com>
2022-06-30 16:51:41 +0700

    base

---
+++ dev/src/pli.ts
@@ -0,0 +1,12 @@
+import bridgeCli from "./bridgecli.js";
+
+const pli = new bridgeCli('hexa', {
+    name: 'Hexa',
+    description: 'A plugin that provides protection against hackers.',
+    author: ['FrostIce482'],
+    version: 'v0.0.1',
+    versionCode: 0.0001,
+    internalModules: {}
+})
+
+export default pli
