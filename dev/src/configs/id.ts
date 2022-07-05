c16219b7dc5e0dd8c3406e845ef84557ce489359
FrostIce482 <frostice48260@gmail.com>
2022-07-05 12:18:00 +0700

    fixes

--- dev/src/configs/id.ts
+++ dev/src/configs/id.ts
@@ -3,6 +3,7 @@ import pli from "../pli.js";
 
 const aa = pli.internalModules['configs/id'] = async (b) => {
     const { scoreboard, storage, plr, server } = await b.import('se')
+    const { world } = await b.import('mc')
     const config = await b.importInternal('libs/config') as libs_config
 
     const cfg = config.nocache(scoreboard.objective.for(`HX:ID:${storage.instance.default.uniqueID.slice(0, 10)}`).dummies)
@@ -13,6 +14,7 @@ const aa = pli.internalModules['configs/id'] = async (b) => {
     server.ev.playerJoin.subscribe(({name, uid}) => {
         if (uid !== -1 && !( name in cfg )) cfg[name] = uid
     })
+    for (const {name, uid} of world.getPlayers()) cfg[name] = uid
 
     return cfg
 }
