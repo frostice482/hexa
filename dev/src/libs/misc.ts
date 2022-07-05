721929570a3278b91d475b8fe9b8124004fc7913
FrostIce482 <frostice48260@gmail.com>
2022-07-05 17:15:06 +0700

    kick function

--- dev/src/libs/misc.ts
+++ dev/src/libs/misc.ts
@@ -1,9 +1,11 @@
 import type { Player } from "mojang-minecraft";
+import { config_common } from "../configs/common.js";
 import pli from "../pli.js";
 
 const aa = pli.internalModules['libs/misc'] = async (b) => {
     const { world } = await b.import('mc')
-    const { permission } = await b.import('se')
+    const { permission, execCmd } = await b.import('se')
+    const ccfg = await b.importInternal('configs/common') as Awaited<config_common>
 
     const getAdmins = function* (excludeAdmins: Player[] = [], minLevel = 60) {
         for (const plr of world.getPlayers())
@@ -11,7 +13,17 @@ const aa = pli.internalModules['libs/misc'] = async (b) => {
                 yield plr
     }
 
+    const kick = (plr: Player, messsage: string | string[]) => {
+        try {
+            if (!( ccfg['kick:useKickCommand'] ??= 1 )) throw 0
+            execCmd(`kick ${JSON.stringify(plr.name)} ${ Array.isArray(messsage) ? messsage.join('\n\u00a7r') : messsage }`)
+        } catch {
+            plr.triggerEvent('se:kick')
+        }
+    }
+
     return {
+        kick,
         getAdmins
     }
 }
