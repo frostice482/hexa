059bf09586ce03c214d2cbbe836849cb7170388c
FrostIce482 <frostice48260@gmail.com>
2022-07-05 17:49:44 +0700

    some fix

--- dev/src/libs/config.ts
+++ dev/src/libs/config.ts
@@ -7,33 +7,33 @@ const aa = pli.internalModules['libs/config'] = () => {
     return Object.assign(
         (dummies: objective['dummies']) => {
             const cache: Record<string, number> = Object.create(null)
-            for (const [name, score] of dummies.getScores()) cache[JSON.parse(`"${name}"`)] = score
+            for (const [name, score] of dummies.getScores()) cache[JSON.parse(`"${name.substring(2)}"`)] = score
             return new Proxy(cache, {
                 set: (t, p, v) => {
                     if (typeof p == 'symbol') return true
-                    dummies.set(p, t[p] = ~~+v)
+                    dummies.set('§r' + p, t[p] = ~~+v)
                     return true
                 },
                 deleteProperty: (t, p) => {
                     if (typeof p == 'symbol') return true
                     delete t[p]
-                    dummies.delete(p)
+                    dummies.delete('§r' + p)
                     return true
                 }
             })
         }, {
             nocache: (dummies: objective['dummies']) => {
                 return new Proxy(Object.create(null) as Record<string, number>, {
-                    get: (t, p) => typeof p == 'symbol' ? undefined : dummies.get(p),
+                    get: (t, p) => typeof p == 'symbol' ? undefined : dummies.get('§r' + p),
                     set: (t, p, v) => {
                         if (typeof p == 'symbol') return true
-                        dummies.set(p, ~~+v)
+                        dummies.set('§r' + p, ~~+v)
                         return true
                     },
-                    has: (t, p) => typeof p == 'symbol' ? false : dummies.exist(p),
+                    has: (t, p) => typeof p == 'symbol' ? false : dummies.exist('§r' + p),
                     deleteProperty: (t, p) => {
                         if (typeof p == 'symbol') return true
-                        dummies.delete(p)
+                        dummies.delete('§r' + p)
                         return true
                     }
                 })
