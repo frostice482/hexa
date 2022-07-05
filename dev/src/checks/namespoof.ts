2b60bd7a39d67ee2250ed4cd71fa79f870e29232
FrostIce482 <frostice48260@gmail.com>
2022-07-05 20:55:39 +0700

    namespoof (illegal name)

--- dev/src/checks/namespoof.ts
+++ dev/src/checks/namespoof.ts
@@ -7,7 +7,7 @@ import { libs_module } from "../libs/module.js";
 import pli from "../pli.js";
 
 pli.internalModules['checks/namespoof'] = async (b) => {
-    const { permission, plr, server, sendChat: { sendMsgToPlayers } } = await b.import('se')
+    const { permission, plr, server, sendChat: { sendMsgToPlayers }, misc: { parseRegex } } = await b.import('se')
     const mlist = await b.importInternal('checks/list') as Awaited<config_common>
     const ccfg = await b.importInternal('configs/common') as Awaited<config_common>
     const icfg = await b.importInternal('configs/id') as Awaited<config_id>
@@ -19,14 +19,24 @@ pli.internalModules['checks/namespoof'] = async (b) => {
     ccfg['ns:checkUID'] ??= true
     ccfg['ns:checkNameLength'] ??= true
     ccfg['ns:maxNameLength'] ??= 16
+    ccfg['ns:checkIllegalName'] ??= true
+    let illegalNameRegex = parseRegex(ccfg['ns:illegalNameRegex'] ??= '/["\\\\]/')
+    Object.defineProperty(ccfg, 'ns:illegalNameRegex', {
+        get: () => String(illegalNameRegex),
+        set: (v) => illegalNameRegex = parseRegex(v ??= '')
+    })
 
     const tests: Record<string, (plr: Player, ctrl: eventControl) => boolean> = {
         nameLength: (plr, ctrl) => {
-            console.warn(
-                ccfg['ns:checkNameLength'],
-                plr.name.length,
-                ccfg['ns:maxNameLength']
-            )
+            if ( ccfg['ns:checkIllegalName'] && illegalNameRegex.test(plr.name) ) {
+                sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name}§r from server: §cNamespoof§r §7(Illegal name)§r`)
+                kick(plr, [])
+                addCancelList(plr, ctrl)
+                return true
+            }
+            return false
+        },
+        illegalName: (plr, ctrl) => {
             if ( ccfg['ns:checkNameLength'] && plr.name.length > ccfg['ns:maxNameLength'] ) {
                 sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name.substring(0, ccfg['ns:maxNameLength'])}§r from server: §cNamespoof§r §7(Name length exceeded)§r §8(length: §2${plr.name.length}§8, max length: §2${ccfg['ns:maxNameLength']}§8)`)
                 kick(plr, [])
@@ -36,12 +46,6 @@ pli.internalModules['checks/namespoof'] = async (b) => {
             return false
         },
         uid: (plr, ctrl) => {
-            console.warn(
-                ccfg['ns:checkUID'],
-                plr.uid,
-                plr.name in icfg,
-                icfg[plr.name]
-            )
             if ( ccfg['ns:checkUID'] && plr.uid !== -1 && plr.name in icfg && icfg[plr.name] !== plr.uid ) {
                 sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name}§r from server: §cNamespoof§r §7(UID mismatch)§r §8(player UID: §2${plr.uid}§8, expected UID: §2${icfg[plr.name]}§8)`)
                 kick(plr, [])
@@ -61,6 +65,7 @@ pli.internalModules['checks/namespoof'] = async (b) => {
     const aa = server.ev.playerJoin.subscribe((plr, ctrl) => {
         if (permission.getLevel(plr.getTags()) > 60) return
         
+        tests.illegalName(plr, ctrl)
         tests.nameLength(plr, ctrl)
         tests.uid(plr, ctrl)
     }, 1000)
