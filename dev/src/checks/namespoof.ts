7d91cb27140ac59d22ba6e2abc01b4759fd360a7
FrostIce482 <frostice48260@gmail.com>
2022-07-06 15:54:52 +0700

    added kick options & changed kick message

--- dev/src/checks/namespoof.ts
+++ dev/src/checks/namespoof.ts
@@ -1,21 +1,27 @@
 import { Player } from "mojang-minecraft";
 import type { eventControl } from "../../types/se/evmngr.js";
+import { config_banlist } from "../configs/banlist.js";
+import { config_blacklist } from "../configs/blacklist.js";
 import { config_common } from "../configs/common.js";
 import { config_id } from "../configs/id.js";
-import { libs_misc } from "../libs/misc.js";
+import { kickConfig, libs_misc } from "../libs/misc.js";
 import { libs_module } from "../libs/module.js";
 import pli from "../pli.js";
 
 pli.internalModules['checks/namespoof'] = async (b) => {
     const { permission, plr, server, sendChat: { sendMsgToPlayers }, misc: { parseRegex } } = await b.import('se')
     const mlist = await b.importInternal('checks/list') as Awaited<config_common>
+    const bancfg = await b.importInternal('configs/banlist') as Awaited<config_banlist>
+    const blackcfg = await b.importInternal('configs/blacklist') as Awaited<config_blacklist>
     const ccfg = await b.importInternal('configs/common') as Awaited<config_common>
     const icfg = await b.importInternal('configs/id') as Awaited<config_id>
-    const { kick, getAdmins } = await b.importInternal('libs/misc') as Awaited<libs_misc>
+    const { kick } = await b.importInternal('libs/misc') as Awaited<libs_misc>
 
     const Module = await b.importInternal('libs/module') as Awaited<libs_module>
     const module = mlist['namespoof'] = new Module('namespoof', 'Namespoof', true)
 
+    ccfg['ns:actionType'] ??= 'ban' // kick | ban | blacklist
+    ccfg['ns:banDuration'] ??= 31449600 // ban duration
     ccfg['ns:checkUID'] ??= true
     ccfg['ns:checkNameLength'] ??= true
     ccfg['ns:maxNameLength'] ??= 16
@@ -26,30 +32,35 @@ pli.internalModules['checks/namespoof'] = async (b) => {
         set: (v) => illegalNameRegex = parseRegex(v ??= '')
     })
 
+    const begone = (plr: Player, ctrl: eventControl, reason: string) => {
+        if (ccfg['ns:actionType'] == 'ban') bancfg[plr.uid] = Date.now() + ccfg['ns:banDuration'] * 1000
+        if (ccfg['ns:actionType'] == 'blacklist') blackcfg[plr.name] = plr.uid
+        kick(plr, {
+            type: ccfg['ns:actionType'],
+            banDuration: ccfg['ns:banDuration'],
+            reason: reason,
+        })
+        ctrl.break()
+    }
+
     const tests: Record<string, (plr: Player, ctrl: eventControl) => boolean> = {
-        nameLength: (plr, ctrl) => {
+        illegalName: (plr, ctrl) => {
             if ( ccfg['ns:checkIllegalName'] && illegalNameRegex.test(plr.name) ) {
-                sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name}§r from server: §cNamespoof§r §7(Illegal name)§r`)
-                kick(plr, [])
-                ctrl.break()
+                begone(plr, ctrl, `§cNamespoof§r §7(Illegal name)§r`)
                 return true
             }
             return false
         },
-        illegalName: (plr, ctrl) => {
+        nameLength: (plr, ctrl) => {
             if ( ccfg['ns:checkNameLength'] && plr.name.length > ccfg['ns:maxNameLength'] ) {
-                sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name.substring(0, ccfg['ns:maxNameLength'])}§r from server: §cNamespoof§r §7(Name length exceeded)§r §8(length: §2${plr.name.length}§8, max length: §2${ccfg['ns:maxNameLength']}§8)`)
-                kick(plr, [])
-                ctrl.break()
+                begone(plr, ctrl, `§cNamespoof§r §7(Name length exceeded)§r §8(length: §2${plr.name.length}§8, max length: §2${ccfg['ns:maxNameLength']}§8)`)
                 return true
             }
             return false
         },
         uid: (plr, ctrl) => {
             if ( ccfg['ns:checkUID'] && plr.uid !== -1 && plr.name in icfg && icfg[plr.name] !== plr.uid ) {
-                sendMsgToPlayers(getAdmins(), `§6[§eHEXA§6]§r Kicked §b${plr.name}§r from server: §cNamespoof§r §7(UID mismatch)§r §8(player UID: §2${plr.uid}§8, expected UID: §2${icfg[plr.name]}§8)`)
-                kick(plr, [])
-                ctrl.break()
+                begone(plr, ctrl, `§cNamespoof§r §7(UID mismatch)§r §8(player UID: §2${plr.uid}§8, expected UID: §2${icfg[plr.name]}§8)`)
                 return true
             }
             return false
--- dev/src/libs/misc.ts
+++ dev/src/libs/misc.ts
@@ -2,9 +2,19 @@ import type { Player } from "mojang-minecraft";
 import { config_common } from "../configs/common.js";
 import pli from "../pli.js";
 
+export type kickConfig = {
+    useTemplate?: boolean
+    type?: 'kick' | 'ban' | 'blacklist'
+    banDuration?: number,
+    moderator?: Player | string
+    reason?: string
+    announceLevel?: 'none' | 'admin' | 'all'
+    announceMessage?: string
+}
+
 const aa = pli.internalModules['libs/misc'] = async (b) => {
     const { world } = await b.import('mc')
-    const { permission, execCmd } = await b.import('se')
+    const { permission, execCmd, misc: { convertToReadableTime }, sendChat: { sendMsgToPlayers, sendMsg } } = await b.import('se')
     const ccfg = await b.importInternal('configs/common') as Awaited<config_common>
 
     const getAdmins = function* (excludeAdmins: Player[] = [], minLevel = 60) {
@@ -15,10 +25,32 @@ const aa = pli.internalModules['libs/misc'] = async (b) => {
 
     ccfg['kick:useKickCommand'] ??= 1
 
-    const kick = (plr: Player, messsage: string | string[]) => {
+    const kick = (plr: Player, messsage: string | string[] | kickConfig) => {
+        const {
+            useTemplate = true,
+            type = 'kick',
+            banDuration = 0,
+            moderator = '[System]',
+            reason = 'N/A',
+            announceLevel = 'admin',
+            announceMessage = ''
+        } = typeof messsage == 'string' ? { reason: messsage }
+            : Array.isArray(messsage) ? { useTemplate: false, reason: messsage.join('\n§r') }
+            : messsage
+
+        const kickType = type == 'blacklist' ? '§4blacklisted§r' : type == 'ban' ? '§cbanned§r' : '§ckicked§r', 
+            modName = typeof moderator == 'string' ? moderator : moderator.name,
+            aa = `from${ type != 'kick' ? ' playing in ' : ' ' }this server${ type == 'ban' ? ` for §a${convertToReadableTime(banDuration, false)}§r` : '' }`
+        
+        if (announceLevel != 'none') {
+            const announceMessageConvert = announceMessage || `§6[§eHEXA§6]§r §b${modName}§r ${kickType} §b${plr.nickname}§r ${aa}: ${reason}`
+            if (announceLevel == 'admin') sendMsgToPlayers(getAdmins(), announceMessageConvert)
+            else sendMsg('@a', announceMessageConvert)
+        }
+        
         try {
             if (!ccfg['kick:useKickCommand']) throw 0
-            execCmd(`kick ${JSON.stringify(plr.name)} ${ Array.isArray(messsage) ? messsage.join('\n\u00a7r') : messsage }`)
+            execCmd(`kick ${JSON.stringify(plr.name)} ${ useTemplate ? [ `You have been ${kickType} ${aa}.`, `Moderator: §b${modName}`, `Reason: ${reason}` ].join('\n§r') : reason} `)
         } catch {
             plr.triggerEvent('se:kick')
         }
