ce0bdfba53a57e3f4421ec94a1437fb7a13e4686
FrostIce482 <frostice48260@gmail.com>
2022-07-07 12:03:39 +0700

    update config/blacklist

--- dev/src/configs/blacklist.ts
+++ dev/src/configs/blacklist.ts
@@ -5,7 +5,7 @@ const aa = pli.internalModules['configs/blacklist'] = async (b) => {
     const { scoreboard, storage } = await b.import('se')
     const config = await b.importInternal('libs/config') as libs_config
 
-    return config.nocache(scoreboard.objective.for(`HX:BK:${storage.instance.default.uniqueID.slice(0, 10)}`).dummies)
+    return config.nocache(scoreboard.objective.for(`HX:BK:${storage.instance.default.uniqueID.slice(0, 10)}`).dummies) as Record<number, number>
 }
 
 export type config_blacklist = ReturnType<typeof aa>
